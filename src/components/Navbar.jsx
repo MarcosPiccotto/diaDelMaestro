71257d65d4e762dd84f46d3e4d78c509575f1089
marcos <marcospiccotto12@gmail.com>
2022-08-01 08:03:20 -0300

    Retouch on the css of the cards

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ export default function Navbar() {
     return(
         <div className="navbar">
             <img className="navbar-logo"src={WorldLogo} alt="World logo" />
-            <span className="navbar-title">my travel journal</span>
+            <span className="navbar-title">My travel journal</span>
         </div>
     );
 }
--- src/data.js
+++ src/data.js
@@ -11,12 +11,32 @@ export default [
     },
     {
         id:2,
-        title: "Mount Fuji",
-        location: "Japan",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+        title: "Sydney Opera House",
+        location: "Australia",
+        googleMapsUrl: "https://goo.gl/maps/iFJT4RzH7DeBzKRh6",
+        startDate: "12 Feb, 2008",
+        endDate: "20 Feb, 2008",
+        description: "Important arts center, which dominates the skyline and offers opera, theater, music, dance, and guided tours.",
+        imageUrl: "https://source.unsplash.com/JmuyB_LibRo"
+    },
+    {
+        id:3,
+        title: "Geirangerfjord",
+        location: "Norway",
+        googleMapsUrl: "https://goo.gl/maps/9wqe9Vtw4YPP4TkH6",
+        startDate: "24 July, 2020",
+        endDate: "12 Aug, 2020",
+        description: "The Geiranger Fjord is a fjord in the Sunnmore region of More of Romsdal county, Norway. It's located entirely in the Stranda Municipality.",
+        imageUrl: "https://source.unsplash.com/3PeSjpLVtLg"
+    },
+    {
+        id:4,
+        title: "Geirangerfjord",
+        location: "Norway",
+        googleMapsUrl: "https://goo.gl/maps/9wqe9Vtw4YPP4TkH6",
+        startDate: "24 July, 2020",
+        endDate: "12 Aug, 2020",
+        description: "The Geiranger Fjord is a fjord in the Sunnmore region of More of Romsdal county, Norway. It's located entirely in the Stranda Municipality.",
+        imageUrl: "https://source.unsplash.com/3PeSjpLVtLg"
     }
 ]
