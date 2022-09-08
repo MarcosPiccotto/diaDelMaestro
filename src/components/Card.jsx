3920a2739f79b10695b7197ff545ae51ba58fd96
Marcos <marcospiccotto12@gmail.com>
2022-09-07 22:56:27 -0300

    colocando lo basico

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,15 @@
 export default function Card(props) {
     console.log(props)
-    const {id,title,location,googleMapsUrl,startDate,endDate,description,imageUrl} = props
+    const {id,title,authors,scratchUrl,description,imageUrl} = props
     return(
         <div id={id} className="card">
-            <img className="card-image" src={imageUrl} alt="Landscape of autralia" />
+            <img className="card-image" src={`src/assets/${imageUrl}.png`} alt="Project photo" />
             <div>
-                <span className="card-location-text"><img className="card-pin" src="src\assets\PinLocation.png" alt=""/>{location}</span>
-                <a href={googleMapsUrl} className="card-googlemaps">View on Google Maps</a>
                 <p>{title}</p>
-                <span className="card-date">{startDate} - <span>{endDate}</span></span>
+                <p className="card-text">Autor/es: {authors}</p>
                 <p className="card-text">{description}</p>
+                <p className="card-text">Link proyecto: <a href={scratchUrl} className="card-text">{scratchUrl}</a></p>
             </div>
-
         </div>
     );
 }
--- src/data.js
+++ src/data.js
@@ -1,42 +1,66 @@
 export default [
     {
         id:1,
-        title: "Mount Fuji",
-        location: "Japan",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+        title: "Copa Renault",
+        authors: "Gorozito",
+        description: "Un pequeño resumen de nuestra increible copa renault",
+        scratchUrl: "https://scratch.mit.edu/projects/729348649",
+        imageUrl: "1"
     },
     {
         id:2,
-        title: "Sydney Opera House",
-        location: "Australia",
-        googleMapsUrl: "https://goo.gl/maps/iFJT4RzH7DeBzKRh6",
-        startDate: "12 Feb, 2008",
-        endDate: "20 Feb, 2008",
-        description: "Important arts center, which dominates the skyline and offers opera, theater, music, dance, and guided tours.",
-        imageUrl: "https://source.unsplash.com/JmuyB_LibRo"
+        title: "Ricardo Stefanini",
+        authors: "Gomez y Villafañe",
+        description: "Hablamos sobre nuestro mejor profe",
+        scratchUrl: "https://scratch.mit.edu/projects/729343877",
+        imageUrl: "2"
     },
     {
         id:3,
-        title: "Geirangerfjord",
-        location: "Norway",
-        googleMapsUrl: "https://goo.gl/maps/9wqe9Vtw4YPP4TkH6",
-        startDate: "24 July, 2020",
-        endDate: "12 Aug, 2020",
-        description: "The Geiranger Fjord is a fjord in the Sunnmore region of More of Romsdal county, Norway. It's located entirely in the Stranda Municipality.",
-        imageUrl: "https://source.unsplash.com/3PeSjpLVtLg"
+        title: "Dia del maestro",
+        authors: "Diserio y Heredia",
+        description: "Feliz dia a todos los profes",
+        scratchUrl: "https://scratch.mit.edu/projects/729343490",
+        imageUrl: "3"
     },
     {
         id:4,
-        title: "Geirangerfjord",
-        location: "Norway",
-        googleMapsUrl: "https://goo.gl/maps/9wqe9Vtw4YPP4TkH6",
-        startDate: "24 July, 2020",
-        endDate: "12 Aug, 2020",
-        description: "The Geiranger Fjord is a fjord in the Sunnmore region of More of Romsdal county, Norway. It's located entirely in the Stranda Municipality.",
-        imageUrl: "https://source.unsplash.com/3PeSjpLVtLg"
+        title: "Entrenamiento Goico",
+        authors: "Gornik",
+        description: "Un excelente gol de nuestro profe",
+        scratchUrl: "https://scratch.mit.edu/projects/729343001",
+        imageUrl: "4"
     }
 ]
+
+// ,
+//     {
+//         id:2,
+//         title: "Sydney Opera House",
+//         location: "Australia",
+//         googleMapsUrl: "https://goo.gl/maps/iFJT4RzH7DeBzKRh6",
+//         startDate: "12 Feb, 2008",
+//         endDate: "20 Feb, 2008",
+//         description: "Important arts center, which dominates the skyline and offers opera, theater, music, dance, and guided tours.",
+//         imageUrl: "https://source.unsplash.com/JmuyB_LibRo"
+//     },
+//     {
+//         id:3,
+//         title: "Geirangerfjord",
+//         location: "Norway",
+//         googleMapsUrl: "https://goo.gl/maps/9wqe9Vtw4YPP4TkH6",
+//         startDate: "24 July, 2020",
+//         endDate: "12 Aug, 2020",
+//         description: "The Geiranger Fjord is a fjord in the Sunnmore region of More of Romsdal county, Norway. It's located entirely in the Stranda Municipality.",
+//         imageUrl: "https://source.unsplash.com/3PeSjpLVtLg"
+//     },
+//     {
+//         id:4,
+//         title: "Geirangerfjord",
+//         location: "Norway",
+//         googleMapsUrl: "https://goo.gl/maps/9wqe9Vtw4YPP4TkH6",
+//         startDate: "24 July, 2020",
+//         endDate: "12 Aug, 2020",
+//         description: "The Geiranger Fjord is a fjord in the Sunnmore region of More of Romsdal county, Norway. It's located entirely in the Stranda Municipality.",
+//         imageUrl: "https://source.unsplash.com/3PeSjpLVtLg"
+//     }
